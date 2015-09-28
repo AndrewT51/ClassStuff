e59306e9f2989cdbee83f61ba4696f07aa192b77
Andrew Trigg <andrewt51@hotmail.com>
2015-09-27 22:16:27 -0700

    Redid calculator to address stringing results problem

--- calcJscopy.js
+++ calcJscopy.js
@@ -9,14 +9,14 @@ $(document).ready(function(){
 	$('#display').text(total);
 	var $display = $('#display');
 	var alreadyHasDecimalDot = false;
-	var nextOperator;
 
 function numberHandler(nextNumber){
-	 	numAsString = (displayNeedsToBeCleared) ? '' + nextNumber : numAsString + '' + nextNumber; 
+	 	numAsString = (displayNeedsToBeCleared) ? '' + nextNumber : numAsString + '' + nextNumber;
+	 	// if(numAsString.length > 7){ numAsString = numAsString.substring(0,7);}
+	 
 	 	nextOperand = parseFloat(numAsString);
 		$display.text(numAsString);
-		
-
+		// console.log(equationArray);
 		displayNeedsToBeCleared = false;	
 	}
 	
@@ -28,72 +28,64 @@ function numberHandler(nextNumber){
 				displayNeedsToBeCleared = false;
 			}
 			numberHandler($(this).data('id'));
-		}
-		
+					}
 	});
 
 	$('.operator').click(function(){
-		if (nextOperator && equationArray.length>1){
-			console.log('next operator :', nextOperator)
-			equationArray[1] = nextOperator;
-			nextOperator = '';
-		}else{
-			theOperator =  $(this).data('id');
-			
-		}
-		if (equationArray[1] !== theOperator){
-			console.log('different!!!!')
-			 nextOperator = theOperator;
-		}
-
+		var theOperator = $(this).data('id');
 	
-		console.log('This is the operator: ' + theOperator);
-	
+		console.log('This is the operator ' + equationArray);
+		// equationArray[1] = theOperator;
 		if (nextOperand) {
 			equationArray.push(nextOperand);
-		
+			console.log('next operand' + equationArray);
 
 		}
 		nextOperand = 0;
-	
-		console.log('Equ array length :' , equationArray.length);
+		// console.log(nextOperand);
+		// console.log(equationArray);
 
 		if (equationArray.length > 0){
 
 			if(equationArray.length === 1){
 				equationArray.push(theOperator);
-			
+				console.log('length 1');
 			}	
 			if(equationArray.length === 2){
+				console.log('length 2');
 				equationArray.pop();
+				// console.log(equationArray);
 				equationArray.push(theOperator);	
+				// console.log(equationArray);
+				console.log('The operator again ' + equationArray[1]);
+			
 			}
 
 			if (equationArray.length === 3 || theOperator === 'percent' || theOperator === 'negate'){
 				
-				// equationArray[1]= theOperator;
+			
 				switch (equationArray[1]){
 
 					case 'add':	
-					// console.log('length 3');
-					// console.log(equationArray);	
-					// console.log('The operator: ' + theOperator);
+					console.log('length 3');
+					console.log(equationArray);	
+					console.log('The operator: ' + theOperator);
 
 					equationArray[0]= equationArray[0] + equationArray[2];
-					// console.log(equationArray.length);
+					console.log(equationArray.length);
 					// if (equationArray.length > 2){
 					// 	equationArray.pop();
 					// }
 					equationArray.pop();
-					// console.log(equationArray.length);
+					console.log(equationArray.length);
 					// console.log('in switch case add');
 					
 
 					break;
 
 					case 'minus':	
-					// console.log('length 3');
-					// console.log(equationArray);	
+					console.log('length 3');
+					console.log(equationArray);	
 					equationArray[0]= equationArray[0] - equationArray[2];
 					equationArray.pop(2);
 					// console.log('in switch case - minus');	
@@ -139,7 +131,6 @@ function numberHandler(nextNumber){
 				}
 			}
 			console.log(equationArray);
-
 		
 			$display.text(equationArray[0]);
 		
---
+++ calculator2.js
@@ -0,0 +1,128 @@
+$(document).ready(init);
+
+function init(){
+var numArr =[];
+console.log(numArr);
+var total = 0;
+var partialSum ='';
+var operator = '';
+var onDisplayNum = '';
+var lastDigitPressed='';
+var decimalDotAlreadyUsed = false;
+var $display = $('#display');
+var theCalc = '';
+var changeOperator = false;
+var hasNumberToProcess = false;
+// var doNegate = false;
+
+$display.text(0);
+
+
+$('.number').click(function(){
+	lastDigitPressed = $(this).data('id');
+	if (lastDigitPressed !== '.' && onDisplayNum.length < 8){
+		onDisplayNum = onDisplayNum + lastDigitPressed;
+		$display.text(onDisplayNum);
+	}else if (lastDigitPressed === '.'){
+		onDisplayNum = decimalDotAlreadyUsed ? onDisplayNum : onDisplayNum + '.';
+		decimalDotAlreadyUsed = true;
+		$display.text(onDisplayNum);
+	}
+
+	changeOperator =true;
+	hasNumberToProcess =true;
+	console.log(numArr)
+})
+
+$('.otherOperator').click(function(){
+	if (hasNumberToProcess){
+		otherOperator = $(this).data('id');
+		if (otherOperator === 'negate'){
+			console.log(numArr)
+			onDisplayNum = parseFloat($display.text()) * -1;
+		}else {
+			onDisplayNum = parseFloat($display.text()) / 100;
+		}
+		$display.text(onDisplayNum);
+	}
+
+})
+
+$('.operator').click(function(){
+		operator = $(this).data('id')
+
+		if (changeOperator){
+			numArr.push(parseFloat(onDisplayNum));
+
+			changeOperator=false;
+
+			if (theCalc && numArr.length===2){
+				console.log('in the calc : ', numArr)
+				total = theCalc(numArr[0],numArr[1]);
+				numArr.pop();
+				numArr[0] = total;
+				if (Math.floor($display.text()) == $display.text()){
+
+				}
+				$display.text(total.toFixed(2).toString().replace(/([0-9]+(\.[1-9]+)?)(\.?0+$)/,"$1"));
+
+			}
+			theCalc='';
+			decimalDotAlreadyUsed =false;
+			onDisplayNum = '';
+			num1 ='';
+		}
+
+			switch (operator){
+
+				case 'add':
+					theCalc = function(num1,num2){
+					 return num1 + num2; 
+					}
+					console.log('num 1: ' , num1);
+				break;
+				case 'minus':
+					theCalc= function(num1,num2){
+					 return num1 - num2; 
+					}
+				break;
+				case 'divide':
+					theCalc= function(num1,num2){
+					 return num1 / num2; 
+					}
+				break;
+				case 'multiply':
+					theCalc= function(num1,num2){
+					 return num1 * num2; 
+					}
+				break;
+
+				case 'equals':
+					$display.text(total.toFixed(2).toString().replace(/([0-9]+(\.[1-9]+)?)(\.?0+$)/,"$1"));
+
+				// console.log('total:', total);
+				break;
+
+		}
+		hasNumberToProcess=false;
+
+})
+
+
+	$('#AC').click(function(){
+		numArr=[];
+		console.log('Entered AC: ' + numArr);
+		total = 0;
+		partialSum ='';
+		operator = '';
+		onDisplayNum = '';
+		lastDigitPressed='';
+		decimalDotAlreadyUsed = false;
+		$display = $('#display');
+		theCalc = '';
+		changeOperator = false;
+		num1=0;
+		$display.text(0);
+ 		
+	});
+}
